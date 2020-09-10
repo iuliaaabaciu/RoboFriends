d1dcd4d308fa8218d25c65ea7440d961faa5295d
iuliab <iuliaiuliabaciu@yahoo.com>
2020-09-10 17:36:38 +0300

    Updates

---
+++ precache-manifest.12363f23c6fc4f5c747fc2d9b6f8e395.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7df647a8493621aa83c39a380c0909",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ada0a3ec4073adf220b4",
+    "url": "/RoboFriends/static/css/main.95bd4d7c.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/RoboFriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada0a3ec4073adf220b4",
+    "url": "/RoboFriends/static/js/main.33820335.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);
